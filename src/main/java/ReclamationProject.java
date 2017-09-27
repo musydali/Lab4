37661cca206dee0440e16277dd979e7bf44207fc
Mustafa Syed Ali <Mustafa Syed Ali@DESKTOP-A5QI6HT>
2017-09-27 12:35:58 -0500

    Fixed the Reclamation Project.

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,38 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+/**
+ * Reclamation Project.
+ */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+public class ReclamationProject {
+    /**
+     *
+     * @param a as a string
+     * @param b as a string
+     * @return a String
+     */
+    static String doit(final String a,  final String b) {
+        String tempA, tempB, tempC;
+        tempA = a;
+        tempB = b;
+        if (a.length() > b.length()) {
+            String c = a; // TODO set c to a
+            tempA = b; tempB = c;
+            }
+        String r = " ";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+        if (a.regionMatches(i, b, k, j) && j > r.length()) {
+            r = a.substring(i, i + j);
+        } else {
+            r = r;
+        }
+                        }
+                } // Ah yeah
         } return r; }
 }
